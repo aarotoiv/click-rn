d9bcc753f78615271741fc2d2af4595b137f69df
aarotoiv <aaro.toivonen@gmail.com>
2020-02-17 11:30:19 +0200

    main.js and packages

---
+++ components/Main.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+
+class Home extends Component {
+    constructor(props) {
+
+    }
+    componentDidMount() {
+        
+    }
+    componentDidUpdate() {
+        
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+
+            </View>
+        );    
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#000",
+        justifyContent: "center",
+    }
+});
+
+const mapStateToProps = (state) => {
+    return {
+       
+    };
+};
+
+export default connect(mapStateToProps, { socketConnect })(Home);
