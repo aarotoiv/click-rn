fe7110c04b98eae820a7bb3b9d4eeccafef9bd65
aarotoiv <aaro.toivonen@gmail.com>
2020-02-17 11:53:08 +0200

    reducer stuff

--- reducers/gameReducer.js
+++ reducers/gameReducer.js
@@ -1,14 +1,37 @@
 import { 
-    TEST_TYPE
+    TEST_TYPE,
+    SOCKET_CONNECTING,
+    SOCKET_CONNECTED,
+    JOINED_SERVER,
+    YOU_CLICKED,
+    OUT_OF_POINTS,
+    DO_RETRY
 } from '../actions/types';
 
 const INITIAL_STATE = {
-
+    socket: null,
+    points: 0,
+    connecting: false,
+    connected: false,
+    joined: false,
+    ask_retry: false,
 };
 
 export default (state = INITIAL_STATE, action) => {
     switch(action.type) {
-       
+        case SOCKET_CONNECTING: 
+            return {...state, connecting: true};
+        case SOCKET_CONNECTED: 
+            return {...state, connecting: false, connected: true, socket: action.payload};
+        case JOINED_SERVER: 
+            return {...state, joined: true};
+        case YOU_CLICKED: 
+            return {...state, points: state.points + action.payload.points};
+        case OUT_OF_POINTS: 
+            return {...state, points: 0, ask_retry: true};
+        case DO_RETRY:
+            return {...state, points: action.payload};
+
         default:
             return state;
     }
