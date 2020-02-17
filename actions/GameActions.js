b74d44dcfce3abe27ff5089b7acdac7ec3681df2
aarotoiv <aaro.toivonen@gmail.com>
2020-02-17 11:48:26 +0200

    actions

--- actions/GameActions.js
+++ actions/GameActions.js
@@ -1,7 +1,52 @@
 import axios from 'axios';
 import { 
     TEST_TYPE,
+    SOCKET_CONNECTING,
+    SOCKET_CONNECTED
 } from './types';
+import SocketHandler from '../util/SocketHandler'
+
+export const socketConnect = () => {
+    return (dispatch) => {
+        dispatch({
+            type: SOCKET_CONNECTING
+        }); 
+        SocketHandler.initialize()
+        .then((socket) => {
+            dispatch({
+                type: SOCKET_CONNECTED,
+                payload:socket
+            });
+        });
+    }
+}
+
+const joinedServer = (data) => {
+    return {
+        type: JOINED_SERVER,
+        payload: data.points
+    }
+};
+
+const youClicked = (data) => {
+    return {
+        type: YOU_CLICKED,
+        payload: data
+    }
+};
+
+const outOfPoints = () => {
+    return {
+        type: OUT_OF_POINTS
+    };
+}
+
+const doRetry = (data) => {
+    return {
+        type: DO_RETRY,
+        payload: data.points
+    }
+}
 
 
 
